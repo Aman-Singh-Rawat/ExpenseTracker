5c265906b1c6a99bb98a5e0b9746a39b30eff77a
aman rawat <amanrawat6767@gmail.com>
2024-05-29 15:09:09 +0530

    adding some icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -40,6 +43,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/internship/expensetracker/presenter/screen/activity/LaunchActivity.kt
@@ -0,0 +1,36 @@
+package com.internship.expensetracker.presenter.screen.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.internship.expensetracker.R
+
+class LaunchActivity : AppCompatActivity() {
+    private val tvAppName: TextView
+        get() = findViewById(R.id.tvMontra)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val alphaAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in)
+        tvAppName.startAnimation(alphaAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@LaunchActivity, OnboardingActivity::class.java))
+            finish()
+        }, 5000)
+
+    }
+}
