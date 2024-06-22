d718a8645a654230a9088b84e45058598bd00d05
aman rawat <amanrawat6767@gmail.com>
2024-06-22 09:22:58 +0530

    onBackPressed functionality adding

--- app/src/main/java/com/internship/expensetracker/presenter/screen/fragment/detail/DetailTransaction.kt
+++ app/src/main/java/com/internship/expensetracker/presenter/screen/fragment/detail/DetailTransaction.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.internship.expensetracker.R
 import com.internship.expensetracker.databinding.FragmentDetailTransactionBinding
+import com.internship.expensetracker.presenter.screen.activity.HomeContainerActivity
 
 class DetailTransaction : Fragment() {
     private lateinit var binding: FragmentDetailTransactionBinding
@@ -16,5 +17,13 @@ class DetailTransaction : Fragment() {
     ): View {
         binding = FragmentDetailTransactionBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.window?.statusBarColor = resources.getColor(R.color.red, null)
+        binding.icFragDetailArrow.setOnClickListener {
+            (activity as HomeContainerActivity).onBackPressed()
+        }
     }
 }
